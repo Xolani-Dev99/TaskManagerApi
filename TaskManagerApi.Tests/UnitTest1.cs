ac0f60c6cc550044e7d41037244ac9f248bebb50
Xolani-Dev99 <xolanindunge@gmail.com>
2025-07-02 15:55:31 +0200

    Add xUnit tests and CI/CD integration

---
+++ TaskManagerApi.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using TaskManagerApi.Models; 
+
+namespace TaskManagerApi.Tests
+{
+    public class TaskTests
+    {
+        [Fact]
+        public void Task_Should_Have_Title()
+        {
+            // Arrange
+            var task = new TaskItem { Title = "Buy milk" };
+
+            // Act & Assert
+            Assert.Equal("Buy milk", task.Title);
+        }
+    }
+}
